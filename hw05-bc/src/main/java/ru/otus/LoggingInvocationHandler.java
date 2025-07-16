0599d0e4a03ab8b6cf21ea16670c3f721ecb9ca5
serg1971ch <serg1971ch@yandex.ru>
2025-07-16 12:45:39 +0300

    compare by type

--- hw05-bc/src/main/java/ru/otus/LoggingInvocationHandler.java
+++ hw05-bc/src/main/java/ru/otus/LoggingInvocationHandler.java
@@ -2,37 +2,52 @@ package ru.otus;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
-import java.util.logging.Logger;
 
 public class LoggingInvocationHandler implements InvocationHandler {
     private final Object target;
-    private final Set<String> loggedMethods;
-
-    Logger logger = Logger.getLogger(String.valueOf(LoggingInvocationHandler.class));
+    private final Map<String, Set<String>> methodSignatures;
 
     public LoggingInvocationHandler(Object target) {
         this.target = target;
-        this.loggedMethods = new HashSet<>();
+        this.methodSignatures = new HashMap<>();
 
         Method[] methods = target.getClass().getDeclaredMethods();
 
         for (Method method : methods) {
             if (method.isAnnotationPresent(Log.class)) {
-                loggedMethods.add(method.getName());
+                String signature = getMethodSignature(method);
+                methodSignatures.computeIfAbsent(method.getName(), k -> new HashSet<>()).add(signature);
             }
         }
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if (loggedMethods.contains(method.getName())) {
+        String currentSignature = getMethodSignature(method);
+
+        if (methodSignatures.containsKey(method.getName())
+                && methodSignatures.get(method.getName()).contains(currentSignature)) {
+
             String params = formatParams(args);
-            logger.info("executed method: " + method.getName() + ", param: " + params);
+            System.out.println("executed method: " + method.getName() + ", param: " + params);
         }
 
         return method.invoke(target, args);
+    }
+
+    private String getMethodSignature(Method method) {
+        StringBuilder signature = new StringBuilder();
+
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        for (Class<?> type : parameterTypes) {
+            signature.append(type.getName()).append(",");
+        }
+
+        return signature.toString();
     }
 
     private String formatParams(Object[] args) {
