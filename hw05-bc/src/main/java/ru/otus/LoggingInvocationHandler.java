0cb199cdcb602241a27716e2e2f398b84d4f66e8
serg1971ch <serg1971ch@yandex.ru>
2025-07-16 08:02:51 +0300

    logging

--- hw05-bc/src/main/java/ru/otus/LoggingInvocationHandler.java
+++ hw05-bc/src/main/java/ru/otus/LoggingInvocationHandler.java
@@ -4,10 +4,13 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Logger;
 
 public class LoggingInvocationHandler implements InvocationHandler {
     private final Object target;
     private final Set<String> loggedMethods;
+
+    Logger logger = Logger.getLogger(String.valueOf(LoggingInvocationHandler.class));
 
     public LoggingInvocationHandler(Object target) {
         this.target = target;
@@ -26,7 +29,7 @@ public class LoggingInvocationHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         if (loggedMethods.contains(method.getName())) {
             String params = formatParams(args);
-            System.out.println("executed method: " + method.getName() + ", param: " + params);
+            logger.info("executed method: " + method.getName() + ", param: " + params);
         }
 
         return method.invoke(target, args);
